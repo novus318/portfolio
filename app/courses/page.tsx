1c8e53b183b519393809893277e644e42eb2d9a0
novus318 <nizamudheen318@gmail.com>
2024-09-10 20:59:38 +0530

    small changes

--- app/courses/page.tsx
+++ app/courses/page.tsx
@@ -1,8 +1,27 @@
 import Lectures from '@/components/Lectures';
 import { FloatingNav } from '@/components/ui/floating-navbar';
+import { Metadata } from 'next';
 import React from 'react';
 import { FaHome, FaProjectDiagram, FaUser } from 'react-icons/fa';
 import { SiHyperskill } from 'react-icons/si';
+
+export const metadata: Metadata = {
+  title: "Data Structures & Algorithms in Java",
+  description: "This course is designed for beginners and focuses on the fundamental concepts of data structures and algorithms, which are crucial for solving complex problems in technical interviews. You'll learn how to implement and optimize algorithms using Java, preparing you to coding interviews with confidence.",
+  keywords: 'Data Structures, Algorithms, Java, Coding Interview, Technical Interview, Algorithm Optimization, Beginner Java Course, Java Programming, Coding Skills, Problem Solving, DSA in Java',
+  metadataBase: new URL('https://nizamudheen.com'),
+  alternates: {
+    canonical: '/courses',
+    languages: {
+      'en-US': '/en-US',
+      'de-DE': '/de-DE',
+    },
+  },
+  openGraph: {
+    images: '/courses-opengraph-image.png',
+    url: 'https://nizamudheen.com/courses',
+  },
+};
 
 const Courses = () => {
   return (
